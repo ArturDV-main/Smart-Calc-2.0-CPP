700efb4f848133ac35300d8b0a3ca05fd5760589
leftrana <leftrana@student.21-school.ru>
2023-09-20 11:11:55 +0300

    view_start fix

--- src/main.cc
+++ src/main.cc
@@ -12,8 +12,6 @@ int main(int argc, char *argv[]) {
   if (argc && argv) {
     s21::CalcModel calc_model;
     s21::CalcController calc_controller(&calc_model);
-    // ConsoleView console_view(&calc_controller);
-    // console_view.StartEventLoop();
     return viewStart(argc, argv);
   }
   return 1;
